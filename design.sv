69770594fb0fd0dda7e03731c2a1e522d28252db
Mahathi <143727003+mahathivinayak@users.noreply.github.com>
2025-11-29 12:32:53 +0530

    Create design.sv

---
+++ design.sv
@@ -0,0 +1,8 @@
+// Code your design here
+module d_ff(input bit d,clk,reset,preset, output bit q);
+  always_ff@(posedge clk,posedge reset,posedge preset)
+    if(reset) q<=1'b0;
+  else if(preset) q<=1'b1;
+  else if(reset && preset) q<=1'b0;
+  	else q<=d;
+endmodule
