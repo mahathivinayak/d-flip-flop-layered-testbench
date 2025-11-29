086eafaa3ead63883e74c726c3c51b24049f7268
Mahathi <143727003+mahathivinayak@users.noreply.github.com>
2025-11-29 12:35:25 +0530

    Create generator.sv

---
+++ generator.sv
@@ -0,0 +1,29 @@
+class generator;
+  
+  transaction trans; 
+  
+  
+  mailbox gen2driv;  
+  
+  
+    
+  function new(mailbox gen2driv); 
+    this.gen2driv = gen2driv;
+  endfunction
+  
+  
+task main();
+    
+    repeat(1)
+      begin
+        
+    	trans = new();
+        trans.randomize(); 
+        gen2driv.put(trans);
+        
+      end
+    
+  
+  endtask
+  
+endclass
